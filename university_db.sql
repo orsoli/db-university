8309d676ef5ac16f27c9498b9fbcb7dd2b323d5f
orsoli <fljcoli@gmail.com>
2024-10-16 15:58:26 +0200

    first semester, first year

--- university_db.sql
+++ university_db.sql
@@ -4,7 +4,7 @@ FROM `students`
 WHERE `date_of_birth` >= "1990-01-01"
 and `date_of_birth` <= "1990-12-31"
 
--- 2. Select all courses worth more than 10 credits (479)
+-- 2. Select all courses worth more than 10 credits
 SELECT * 
 FROM `courses` 
 WHERE `cfu` > 10
@@ -13,3 +13,9 @@ WHERE `cfu` > 10
 SELECT *
 FROM `students`
 WHERE `date_of_birth` <= "1994-10-16"
+
+-- 4. Select all first semester-courses in the first year of any deegre program
+SELECT * 
+FROM `courses` 
+WHERE `period` = "I semestre"
+and `year` = 1
