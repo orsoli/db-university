4807f4ad338099afc7e22a21d6b0ba6156001b13
orsoli <fljcoli@gmail.com>
2024-10-16 15:42:24 +0200

    courses more then 10 credits

--- university_db.sql
+++ university_db.sql
@@ -3,3 +3,8 @@ SELECT *
 FROM `students`
 WHERE `date_of_birth` >= "1990-01-01"
 and `date_of_birth` <= "1990-12-31"
+
+-- 2. Select all courses worth more than 10 credits (479)
+SELECT * 
+FROM `courses` 
+WHERE `cfu` > 10
