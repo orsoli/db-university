196a2cc320da43d70eac2af8c2e478b5016de15f
orsoli <fljcoli@gmail.com>
2024-10-17 17:19:59 +0200

    Counted teachers same building

--- db_university/group_by_query.sql
+++ db_university/group_by_query.sql
@@ -2,3 +2,8 @@
 SELECT YEAR(`enrolment_date`) as `year`, COUNT(`enrolment_date`) as `ernolment_sum`
 FROM `students` 
 GROUP BY  YEAR(`enrolment_date`) ORDER BY YEAR ASC
+
+-- Count how many teachers have an office in the same building
+SELECT `office_address` as `office_building`, COUNT(`office_address`) as `teacher_nr`
+FROM `teachers`
+GROUP BY `office_address` ORDER BY `office_address` ASC;
